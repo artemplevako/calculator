9acbc91527b53c47cc97c97e94b2f93187d89802
Artem Plevako <artemplevako03@gmail.com>
2021-10-23 10:01:16 +0900

    Connect ui to js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const display = document.querySelector('.display');
+
+const digits = document.querySelectorAll('.digit');
+
+digits.forEach(digit => digit.addEventListener('click', e =>
+    display.textContent += e.target.textContent
+));
+
+const operations = document.querySelectorAll('.operation');
+
+operations.forEach(operation => operation.addEventListener('click', e => {
+    if (display.textContent === '' ||
+            display.textContent.split(' ').length > 1) return;
+    display.textContent += ' ' + e.target.textContent + ' ';
+}));
+
+const equals = document.querySelector('#equals');
+
+equals.addEventListener('click', e => {
+    const [lhs, operand, rhs] = display.textContent.split(' ');
+    if (typeof rhs === 'undefined') return;
+    display.textContent = round(operate(+lhs, +rhs, operand), 4);
+});
+
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', e => display.textContent = '');
+
+function round (n, decimalDigits) {
+    return Math.round(n * 10**decimalDigits) / 10**decimalDigits;
+}
+
 function add (a, b) {
     return a + b;
 }
